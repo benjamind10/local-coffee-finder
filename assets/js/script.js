c3c7980e31e363c35b6b3cedcb86f8a9790542a7
Ben Duran <benjamind10@pm.me>
2022-01-13 00:40:46 -0500

    switched to google maps api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
-mapboxgl.accessToken = config.MAP_BOX;
+// Create the script tag, set the appropriate attributes
+var script = $('<script>');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${config.G_KEY}&callback=initMap`;
+script.async = true;
 
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
-});
+// Attach your callback function to the `window` object
+window.initMap = function () {
+  // JS API is loaded and available
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 37.42778, lng: -77.62199 },
+    zoom: 12,
+  });
+};
+
+// Append the 'script' element to 'head'
+document.head.appendChild(script);
