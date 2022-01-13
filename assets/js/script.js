8a4c096c073977b9ebdf5467a1fb0a8b0da3127a
Ben Duran <benjamind10@pm.me>
2022-01-13 01:33:51 -0500

    added search handler to catch input by user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,28 @@
 // Create the script tag, set the appropriate attributes
-var script = document.createElement('script');
+let script = document.createElement('script');
 script.src = `https://maps.googleapis.com/maps/api/js?key=${config.G_KEY}&callback=initMap`;
 script.async = true;
 
 // Attach your callback function to the `window` object
-window.initMap = function () {
-  var options = {
-    center: { lat: 37.42778, lng: -77.62199 },
-    zoom: 12,
+function initMap() {
+  window.initMap = function () {
+    let options = {
+      center: { lat: 37.42778, lng: -77.62199 },
+      zoom: 12,
+    };
+    // JS API is loaded and available
+    map = new google.maps.Map(
+      document.getElementById('map'),
+      options
+    );
   };
-  // JS API is loaded and available
-  map = new google.maps.Map($('#map'), options);
-};
 
-// Append the 'script' element to 'head'
-document.head.appendChild(script);
+  // Append the 'script' element to 'head'
+  document.head.appendChild(script);
+}
+
+$(document).on('submit', function (event) {
+  event.preventDefault();
+
+  console.log($('#search-address').val().trim());
+});
