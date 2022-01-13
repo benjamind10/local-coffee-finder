0551a74654e4aea3158beea4a99c2f2a728fcca8
Ben Duran <benjamind10@pm.me>
2022-01-13 00:55:19 -0500

    displayed local location on map

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
 // Create the script tag, set the appropriate attributes
-var script = $('<script>');
+var script = document.createElement('script');
 script.src = `https://maps.googleapis.com/maps/api/js?key=${config.G_KEY}&callback=initMap`;
 script.async = true;
 
 // Attach your callback function to the `window` object
 window.initMap = function () {
-  // JS API is loaded and available
-  map = new google.maps.Map(document.getElementById('map'), {
+  var options = {
     center: { lat: 37.42778, lng: -77.62199 },
     zoom: 12,
-  });
+  };
+  // JS API is loaded and available
+  map = new google.maps.Map($('#map'), options);
 };
 
 // Append the 'script' element to 'head'
