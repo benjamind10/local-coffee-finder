c24c16ff540903b3c10f238d5d6957fcbb0b3b7a
Ben Duran <benjamind10@pm.me>
2022-01-14 20:45:39 -0500

    added autocomplete and geolocation button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,81 @@
 // Create the script tag, set the appropriate attributes
-let script = document.createElement('script');
+var script = document.createElement('script');
 script.src = `https://maps.googleapis.com/maps/api/js?key=${config.G_KEY}&callback=initMap&libraries=places`;
 script.async = true;
 
-// Append the 'script' element to 'head'
-document.head.appendChild(script);
-
-// JQuery Selectors
-let searchEl = $('#search-address');
-
 // Global Variables
-let places;
-let pos = {};
-let tmp;
+let map, infoWindow;
+const center = { lat: 37.42778, lng: -77.62199 };
+let tmp = {};
 
-navigator.geolocation.getCurrentPosition(position => {
-  pos = {
-    lat: position.coords.latitude,
-    lng: position.coords.longitude,
-  };
-});
-
-// Event Handlers
-$(document).on('submit', function (event) {
-  event.preventDefault();
-
-  console.log(searchEl.val().trim());
-});
-
-// Functions
 function genMap() {
-  window.initMap = function (pos) {
-    if (!pos) {
-      pos.lat = 37.42778;
-      pos.lng = -77.62199;
-    }
-
-    const options = {
-      center: { lat: pos.lat, lng: pos.lng },
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: center,
       zoom: 12,
-    };
+    });
 
-    // JS API is loaded and available
-    map = new google.maps.Map(
-      document.getElementById('map'),
-      options
+    infoWindow = new google.maps.InfoWindow();
+
+    const locationButton = document.createElement('button');
+
+    locationButton.textContent = 'Go To  Your Location';
+    locationButton.classList.add('custom-map-control-button');
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(
+      locationButton
     );
+    locationButton.addEventListener('click', () => {
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          position => {
+            const pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
 
-    // Marker
-    const marker = new google.maps.Marker({
-      position: { lat: options.center.lat, lng: options.center.lng },
-      map: map,
-    });
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          },
+          () => {
+            handleLocationError(true, infoWindow, map.getCenter());
+          }
+        );
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
 
-    // InfoWindow
-    const detailWindow = new google.maps.InfoWindow({
-      content: `<h2></h2>`,
+      initAutoComplete();
     });
-  };
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? 'Error: The Geolocation service failed.'
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
 }
 
 function initAutoComplete() {
   let autocomplete;
   let input = document.getElementById('search-address');
 
+  const defaultBounds = {
+    north: center.lat + 0.1,
+    south: center.lat - 0.1,
+    east: center.lng + 0.1,
+    west: center.lng - 0.1,
+  };
+
   const options = {
+    bounds: defaultBounds,
     componentRestrictions: { country: 'us' },
     types: ['address'],
     fields: ['address_components', 'geometry', 'icon', 'name'],
@@ -73,21 +84,19 @@ function initAutoComplete() {
   autocomplete = new google.maps.places.Autocomplete(input, options);
 
   autocomplete.addListener('place_changed', () => {
-    places = autocomplete.getPlace();
-
-    if (!places.geometry || !places.geometry.location) {
-      // User entered the name of a Place that was not suggested and
-      // pressed the Enter key, or the Place Details request failed.
-      alert("No details available for input: '" + places.name + "'");
-      return;
-    } else {
-      tmp = places;
-    }
+    const place = autocomplete.getPlace().geometry.location;
+    tmp.lat = place.lat();
+    tmp.lng = place.lng();
+    console.log(tmp);
   });
+  map.setCenter(tmp);
+  return tmp;
 }
 
 function nearbySearch() {
-  const queryUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${pos.lat}%2C${pos.lng}&radius=2000&region=us&type=cafe,bakery&key=${config.G_KEY}`;
+  if ((tmp = {})) tmp = center;
+
+  const queryUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${tmp.lat}%2C${tmp.lng}&radius=2000&region=us&type=cafe,bakery&key=${config.G_KEY}`;
 
   $.ajax({
     url: queryUrl,
@@ -100,3 +109,6 @@ function nearbySearch() {
       console.log(response);
     });
 }
+
+// Append the 'script' element to 'head'
+document.head.appendChild(script);
