afbf8fbcf5c0edb5090fa4e524fd4e34b12ca180
Ben Duran <benjamind10@pm.me>
2022-01-14 21:02:58 -0500

    fully functional map with autocomplete & change location

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,62 +8,46 @@ let map, infoWindow;
 const center = { lat: 37.42778, lng: -77.62199 };
 let tmp = {};
 
-function genMap() {
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: center,
-      zoom: 12,
-    });
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: center,
+    zoom: 12,
+  });
 
-    infoWindow = new google.maps.InfoWindow();
+  infoWindow = new google.maps.InfoWindow();
 
-    const locationButton = document.createElement('button');
+  const locationButton = document.createElement('button');
 
-    locationButton.textContent = 'Go To  Your Location';
-    locationButton.classList.add('custom-map-control-button');
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(
-      locationButton
-    );
-    locationButton.addEventListener('click', () => {
-      // Try HTML5 geolocation.
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          position => {
-            const pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude,
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-          },
-          () => {
-            handleLocationError(true, infoWindow, map.getCenter());
-          }
-        );
-      } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-      }
-
-      initAutoComplete();
-    });
-  }
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? 'Error: The Geolocation service failed.'
-      : "Error: Your browser doesn't support geolocation."
+  locationButton.textContent = 'Go To  Your Location';
+  locationButton.classList.add('custom-map-control-button');
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(
+    locationButton
   );
-  infoWindow.open(map);
-}
+  locationButton.addEventListener('click', () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
 
-function initAutoComplete() {
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Location found.');
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+
   let autocomplete;
   let input = document.getElementById('search-address');
 
@@ -85,12 +69,20 @@ function initAutoComplete() {
 
   autocomplete.addListener('place_changed', () => {
     const place = autocomplete.getPlace().geometry.location;
-    tmp.lat = place.lat();
-    tmp.lng = place.lng();
-    console.log(tmp);
+    center.lat = place.lat();
+    center.lng = place.lng();
+    map.setCenter(center);
   });
-  map.setCenter(tmp);
-  return tmp;
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? 'Error: The Geolocation service failed.'
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
 }
 
 function nearbySearch() {
