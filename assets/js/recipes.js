0dac18c59e6f868ba8c81e965515bc03b00f1c60
blake allen <csgo6811@gmail.com>
2022-01-16 10:29:48 -0500

    added new card feature/fixed typos added html classes

---
+++ assets/js/recipes.js
@@ -0,0 +1,4 @@
+
+ function getRecipe() {     const baseURL = 'https://api.sampleapis.com/coffee/hot';      $.ajax({         url: baseURL,         method: 'GET'     })         .then(function (response) {         console.log(response)         })         .catch(function (error) {         console.log(error)     }) 
+
+ }
