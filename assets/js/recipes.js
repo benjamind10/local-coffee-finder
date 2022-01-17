fb8f768ef45a05f21f8c30e38be1cb41d05480b6
Ben Duran <benjamind10@pm.me>
2022-01-17 12:14:24 -0500

    added random recipe displayer

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -1,10 +1,35 @@
+// Jquery Selectors
+let recipesCard = $('#recipes-card');
+let recipeName = $('#name');
+let ingredients = $('#ingredients');
+let description = $('#description');
+
+let data;
+
+$(function () {
+  getRecipe();
+  console.log(data);
+});
+
 function getRecipe() {
   const baseURL = 'https://api.sampleapis.com/coffee/hot';
   $.ajax({ url: baseURL, method: 'GET' })
     .then(function (response) {
       console.log(response);
+      data = response;
     })
     .catch(function (error) {
       console.log(error);
     });
+}
+
+function genRandomCoffee() {
+  let num = Math.floor(Math.random() * 24);
+  console.log(num);
+
+  recipeName.text(data[num].title);
+  ingredients.text(data[num].ingredients);
+  description.text(data[num].description);
+
+  recipesCard.css('display', 'block');
 }
